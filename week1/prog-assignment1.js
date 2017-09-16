893ceb7778b8eb60850d558004a7b00e6f9aeb42
Aos <aosdab@gmail.com>
2017-09-16 01:53:50 -0400

    Added grade-school algo

---
+++ week1/prog-assignment1.js
@@ -0,0 +1,29 @@
+/*
+ * Week 1 - Programming Assignment #1
+ *
+ * Implement one or more of the integer multiplication algorithms:
+ * a. Recursive integer multiplication
+ *    - Input: two `n`-digit numbers
+ *    - Output: the product `x * y`
+ * b. Karatsuba's algorithm
+*/
+
+function integerMult(numOne, numTwo) {
+  const numOneLength = numOne.toString().length,
+        numTwoLength = numTwo.toString().length;
+
+  if (numTwo === 0) return 0;
+
+  let multiplier = numTwo % 10,
+      result = numOne * multiplier * (10 ** (numOneLength - numTwoLength));
+
+  const nextNum = Math.floor(numTwo / 10);
+
+  return result + integerMult(numOne, nextNum);
+}
+
+function karatsuba(numOne, numTwo) {
+
+}
+
+console.log(integerMult(5211986647570828172548043773141856795012425179839383045438476733, 4606712684935304570646997185447986666259678494439329643332680432));
