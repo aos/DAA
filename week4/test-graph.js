954f2814b271c550a8669aec187a117f41ceae43
Aos <aosdab@gmail.com>
2017-10-07 02:59:07 -0400

    Fixed file implementation

--- week4/test-graph.js
+++ week4/test-graph.js
@@ -1,14 +1,27 @@
+const fs = require ('fs');
 const GraphPrototype = require('./graph.js')
 
 const Graph = new GraphPrototype();
 
-Graph.addVertex('A'); 
-Graph.addVertex('B'); 
-Graph.addVertex('C'); 
+const arr = fs.readFileSync('./kargerMinCut.txt', 'utf-8').split(/\s/);
 
-Graph.addEdge('A', 'B');
-Graph.addEdge('B', 'C');
-Graph.addEdge('C', 'A');
+let newV = 2;
+let prev;
+arr.forEach((x) => {
+  if (newV == 2) {
+    if (x) Graph.addVertex(x);
+    prev = x;
+    newV = 0;
+    return;
+  }
+
+  if (!x) {
+    newV += 1;
+    return;
+  }
+
+  if (x) Graph.addEdge(prev, x);
+})
 
 console.log(Graph.print());
 
