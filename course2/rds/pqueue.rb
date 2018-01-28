f831e6da0b4bf23927b7bed87026aad54536e1c1
Aos <aosdab@gmail.com>
2018-01-27 23:55:37 -0600

    Added priority q implementation in ruby

---
+++ course2/rds/pqueue.rb
@@ -0,0 +1,35 @@
+# Min-priority Q
+class PriorityQueue
+  def initialize
+    @items = []
+  end
+
+  def enqueue(item, priority)
+    ele = Hash[item: item, priority: priority]
+    return @items << ele if @items.length == 0
+
+    @items.each.with_index do |item, index|
+      if ele[:priority] < item[:priority]
+        return @items.insert(index, ele)
+      end
+    end
+    @items << ele
+  end
+
+  def dequeue
+    raise Exception, 'Empty queue' if @items.length == 0
+    @items.shift
+  end
+
+  def empty?
+    @items.length == 0
+  end
+
+  def size
+    @items.length
+  end
+
+  def print
+    @items
+  end
+end
